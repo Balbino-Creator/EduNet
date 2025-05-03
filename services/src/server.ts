5ed305e97ab08f0e448419497ddd0665b65e20d7
Balbino <bmongar0111@g.educaand.es>
2025-05-03 13:16:10 +0200

    basic monolithic set up

---
+++ services/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import router from './router'
+import db from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Successful connection to the database')
+    } catch (error) {
+        console.log(error)
+        console.log('There was an error trying to connect to the database.')
+    }
+}
+
+connectDB()
+
+const server = express()
+
+server.use('/', router)
+
+export default server
